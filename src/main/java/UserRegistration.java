349f1660d7021e93292293f29f3a0a6ee58cd280
Mohd Anwar Shaikh <mohdanwarshaikh9@gmail.com>
2024-06-11 14:41:05 +0530

    UC3-Valid email added

--- src/main/java/UserRegistration.java
+++ src/main/java/UserRegistration.java
@@ -4,11 +4,11 @@ import java.util.regex.Pattern;
 public class UserRegistration {
     public void validateName() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter last name ");
-        String lastName=sc.nextLine();
-        String regex=  "[A-Z][a-z]{4,10}";
+        System.out.println("Enter Email Id ");
+        String emailID=sc.nextLine();
+        String regex=  "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
         Pattern p=Pattern.compile(regex);
-        Matcher m=p.matcher(lastName);
+        Matcher m=p.matcher(emailID);
         boolean b = m.matches();
         System.out.println(b);
 
