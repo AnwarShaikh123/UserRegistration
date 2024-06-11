17c2b8c30739e7293e8fe5abb7e80860c4eb8cbf
Mohd Anwar Shaikh <mohdanwarshaikh9@gmail.com>
2024-06-11 17:28:34 +0530

    UC6-Atleast one Uppercase added

--- src/main/java/UserRegistration.java
+++ src/main/java/UserRegistration.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 public class UserRegistration {
-    public void validateName() {
+    public void validatePassword() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter password : ");
         String password=sc.nextLine();
-        String regex=  "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=]).{8,}$";
+        String regex=  "^(?=.*[A-Z]).{8,}$";
         Pattern p=Pattern.compile(regex);
         Matcher m=p.matcher(password);
         boolean b = m.matches();
@@ -17,6 +17,6 @@ public class UserRegistration {
 
     public static void main(String[] args) {
         UserRegistration ur=new UserRegistration();
-        ur.validateName();
+        ur.validatePassword();
     }
 }
